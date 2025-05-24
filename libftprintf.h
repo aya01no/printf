abb172db9d0c8d819e73820d23cdf7ea0520d1fb
ayayamad <kurimaru0105@gmail.com>
2025-05-24 16:40:13 +0900

    libftprintf.h

---
+++ libftprintf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayayamad <ayayamad@student.42tokyo.jp>     #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-05-24 05:40:23 by ayayamad          #+#    #+#             */
+/*   Updated: 2025-05-24 05:40:23 by ayayamad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+#endif
