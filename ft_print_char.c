331759b78d99e4d69a65535f499d38a958caa1da
aya01no <kurimaru0105@icloud.com>
2025-05-26 22:06:01 +0900

    del count

--- ft_print_char.c
+++ ft_print_char.c
@@ -3,32 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_char.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayayamad <kurimaru@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kurimaru <kurimaru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 13:05:46 by ayayamad          #+#    #+#             */
-/*   Updated: 2025/05/22 14:51:54 by ayayamad         ###   ########.fr       */
+/*   Updated: 2025/05/26 22:04:52 by kurimaru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-size_t	ft_put_char(int c, int *count)
+size_t	ft_put_char(int c)
 {
 	write (1, &c, 1);
-	(*count)++;
 	return (1);
 }
 
-size_t	ft_put_str(char *s, int *count)
+size_t	ft_put_str(char *s)
 {
 	size_t	len;
 
 	if (!s)
-		ft_put_str("(null)", count);
+		ft_put_str("(null)");
 	len = 0;
 	while (s[len])
 	{
-		ft_put_char(s[len], count);
+		ft_put_char(s[len]);
 		len++;
 	}
 	return (len);
