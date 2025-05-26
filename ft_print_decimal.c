f98a66f08857b32e32586233cd30f0d8ddc183a9
aya01no <kurimaru0105@icloud.com>
2025-05-26 22:06:54 +0900

    del count

--- ft_print_decimal.c
+++ ft_print_decimal.c
@@ -1,40 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_signed.c                                  :+:      :+:    :+:   */
+/*   ft_print_decimal.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayayamad <ayayamad@student.42tokyo.jp>     #+#  +:+       +#+        */
+/*   By: kurimaru <kurimaru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-05-26 06:11:04 by ayayamad          #+#    #+#             */
-/*   Updated: 2025-05-26 06:11:04 by ayayamad         ###   ########.fr       */
+/*   Created: 2025/05/26 06:11:04 by ayayamad          #+#    #+#             */
+/*   Updated: 2025/05/26 22:06:42 by kurimaru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-size_t	ft_put_signed_nbr(int c, int *count)
+size_t	ft_put_signed_nbr(int c)
 {
 	size_t	len;
 
 	len = 0;
 	if (c < 0)
 	{
-		len += ft_put_char('-', count);
+		len += ft_put_char('-');
 		c = -c;
 	}
 	if (c >= 10)
-		len += ft_put_signed_nbr((c / 10), count);
-	len = len + ft_put_char((c % 10) + '0', count);
+		len += ft_put_signed_nbr((c / 10));
+	len = len + ft_put_char((c % 10) + '0');
 	return (len);
 }
 
-size_t	ft_put_unsigned_nbr(unsigned int c, int *count)
+size_t	ft_put_unsigned_nbr(unsigned int c)
 {
 	size_t	len;
 
 	len = 0;
 	if (c >= 10)
-		len += ft_put_unsigned_nbr((c / 10), count);
-	len += ft_put_char((c % 10) + '0', count);
+		len += ft_put_unsigned_nbr((c / 10));
+	len += ft_put_char((c % 10) + '0');
 	return (len);
 }
